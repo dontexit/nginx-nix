9da9085cc78b81c69f694ad7861a843b187c3e49
Edwin Hamal Thakuri <edwinhamal@gmail.com>
2022-05-20 23:26:30 +0545

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 # start nginx
+mv -f replit.nix.bak replit.nix | echo "nix already configure"
+
 echo "STARING NGINX SERVER"
+
 pkill nginx
 nginx -e /home/runner/$REPL_SLUG/logs/error.log -c /home/runner/$REPL_SLUG/nginx.conf
 
