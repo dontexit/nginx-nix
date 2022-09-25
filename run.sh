2533ff9ccf35d817be514c148afc6fd9b2f530a9
Edwin Hamal Thakuri <edwinhamal@gmail.com>
2022-09-25 13:29:24 +0545

    vars for dirs

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+
 # start nginx
 mv -f replit.nix.bak replit.nix | echo "nix already configured"
 
@@ -7,7 +8,3 @@ pkill nginx
 BASE_DIR=/home/runner/$REPL_SLUG
 export BASE_DIR
 nginx -e $BASE_DIR/logs/error.log -c /home/runner/$REPL_SLUG/nginx.conf
-
-
-
-
