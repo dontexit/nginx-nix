b7725729ecfce23836494b1fb590e62ed808cc82
Edwin Hamal Thakuri <edwinhamal@gmail.com>
2022-09-25 11:21:53 +0545

    django verion init

--- replit.nix
+++ replit.nix
@@ -1,5 +1,26 @@
-{ pkgs }: {
+{ pkgs }:
+let
+    nginxModified = pkgs.nginx.overrideAttrs (oldAttrs: rec {
+        configureFlags = oldAttrs.configureFlags ++ [
+            "--http-client-body-temp-path=/home/runner/ramrobazar/cache/client_body"
+            "--http-proxy-temp-path=/home/runner/ramrobazar/cache/proxy"
+            "--http-fastcgi-temp-path=/home/runner/ramrobazar/cache/fastcgi"
+            "--http-uwsgi-temp-path=/home/runner/ramrobazar/cache/uwsgi"
+            "--http-scgi-temp-path=/home/runner/ramrobazar/cache/scgi"
+         ];
+    });
+
+in {
     deps = [
-        pkgs.cowsay
-    ];
+        nginxModified
+    pkgs.python310   
+    pkgs.python310Packages.django
+    pkgs.python310Packages.gunicorn
+    pkgs.python310Packages.pillow
+    pkgs.python310Packages.requests
+    pkgs.python310Packages.python-dotenv  
+    pkgs.psmisc
+    pkgs.systemdMinimal
+    pkgs.lsof
+	];
 }
--- run.sh
+++ run.sh
@@ -1,11 +1,26 @@
 # start nginx
-mv -f replit.nix.bak replit.nix | echo "nix already configured"
+
+echo "STARTING.."
+
+echo "KILLING EXISTING SERVERS"
+
+kill  `cat gunicorn/dev.pid`
+# start Python app
+kill -9 $(lsof -t -i:8181)
+echo "STARING GUNICORN SERVER"
+BASE_DIR=/home/runner/$REPL_SLUG
+BASE_PROJ_DIR=$BASE_DIR/PROJECT_NAME
+
+GUNICORN_DIR=$BASE_PROJ_DIR/config/gunicorn/dev.py #remove if not needed or change it to your config
+export BASE_DIR BASE_PROJ_DIR
+
+gunicorn -c GUNICORN_DIR
+
 
 echo "STARING NGINX SERVER"
+kill -9 $(lsof -t -i:8080)
+kill `cat logs/nginx.pid`
+pkil nginx
+nginx -e $BASE_DIR/logs/error.log -c /home/runner/$REPL_SLUG/nginx.conf
 
-pkill nginx
-nginx -e /home/runner/$REPL_SLUG/logs/error.log -c /home/runner/$REPL_SLUG/nginx.conf
-
-
-
-
+tail -f -f $BASE_DIR/logs/error.log
