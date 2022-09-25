64e9a0bb7717ced9e0f37e7524092da95876c73a
Edwin Hamal Thakuri <edwinhamal@gmail.com>
2022-09-25 11:28:23 +0545

    use variable for base dir

--- run.sh
+++ run.sh
@@ -4,7 +4,9 @@ mv -f replit.nix.bak replit.nix | echo "nix already configured"
 echo "STARING NGINX SERVER"
 
 pkill nginx
-nginx -e /home/runner/$REPL_SLUG/logs/error.log -c /home/runner/$REPL_SLUG/nginx.conf
+BASE_DIR=/home/runner/$REPL_SLUG
+export BASE_DIR
+nginx -e $BASE_DIR/logs/error.log -c /home/runner/$REPL_SLUG/nginx.conf
 
 
 
